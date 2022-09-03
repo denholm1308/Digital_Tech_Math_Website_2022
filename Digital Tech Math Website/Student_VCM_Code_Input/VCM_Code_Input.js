e8417464da8cf364677967cfe69871718a912522
deejay <63299826+denholm1308@users.noreply.github.com>
2022-09-04 09:48:51 +1000

    Add files via upload

---
+++ Digital Tech Math Website/Student_VCM_Code_Input/VCM_Code_Input.js
@@ -0,0 +1,13 @@
+//START OF JS CODE --->
+
+function generate_learning_plan(){
+    const codes = new Set(["a","b","c"]);
+   // var codes = ["lol", "poop", "denholm"]
+    var vcm_code = document.getElementById("VCM_textbox")
+    if (vcm_code.value = codes[1]){
+        alert("lol");
+    } else {
+        alert("YES BUDDY");
+    }
+}
+
